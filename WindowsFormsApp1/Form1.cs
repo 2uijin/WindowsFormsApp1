9830659722add214716397b949c55fe683c13bcf
2uijin <49094082+2uijin@users.noreply.github.com>
2020-04-22 10:21:28 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("다음 시간은 운영체제인데요, 정말 재미있겠죠?","안녕하세요.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+    }
+}
